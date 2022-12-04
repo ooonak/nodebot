e93793c1e68948edd34c3bfc570267d2b51bdf06
Casper Thorø Vium Pedersen <kanoo@posteo.de>
2022-12-04 09:10:30 +0100

    Added new config options.

--- src/Config.hpp
+++ src/Config.hpp
@@ -12,9 +12,11 @@ struct Config
   std::string token;
   std::string channelPrefix;
   int channelLifetimeInHours;
+  int maxNodes;
+  int updateFrequencySeconds;
 };  // Config
 
 }  // namespace nb
 
 TOML11_DEFINE_CONVERSION_NON_INTRUSIVE(nb::Config, token, channelPrefix,
-                                       channelLifetimeInHours)
+                                       channelLifetimeInHours, maxNodes, updateFrequencySeconds)
