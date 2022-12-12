0237187bd56bd6e8692dab5da44b7fb62b0ec6fb
Casper Thorø Vium Pedersen <kanoo@posteo.de>
2022-12-12 12:54:04 +0100

    Added processing time information and help command.

--- src/dpp/SlashCommandController.cpp
+++ src/dpp/SlashCommandController.cpp
@@ -19,18 +19,20 @@ void nb::SlashCommandController::start(dpp::snowflake &guildId)
     mBot->on_slashcommand(
         std::bind(&SlashCommandController::onSlashCommand, this, _1));
 
-    dpp::slashcommand command("cmd", "Send a command", mBot->me.id);
+    dpp::slashcommand helpCommand("help", "Print help message", mBot->me.id);
+
+    dpp::slashcommand cmdCommand("cmd", "Send a command", mBot->me.id);
 
     auto idOption = dpp::command_option(
         dpp::co_integer, "id",
         "The ID of the node as dislayed in it's embed message.", true);
     idOption.set_min_value(dpp::command_option_range{0});
-    command.add_option(idOption);
+    cmdCommand.add_option(idOption);
 
     auto cmdOption =
         dpp::command_option(dpp::co_string, "cmd", "Command to invoke.", true);
     cmdOption.set_max_length(dpp::command_option_range{256});
-    command.add_option(cmdOption);
+    cmdCommand.add_option(cmdOption);
 
     for (size_t i = 0; i < NumberOfArgs; ++i)
     {
@@ -38,11 +40,15 @@ void nb::SlashCommandController::start(dpp::snowflake &guildId)
           dpp::co_string, "arg" + std::to_string(i),
           "Optional argument " + std::to_string(i + 1) + "to command.", false);
       argOption.set_max_length(dpp::command_option_range{256});
-      command.add_option(argOption);
+      cmdCommand.add_option(argOption);
     }
 
     mBot->guild_command_create(
-        command, guildId,
+        helpCommand, guildId,
+        std::bind(&SlashCommandController::onCommandCreated, this, _1));
+
+    mBot->guild_command_create(
+        cmdCommand, guildId,
         std::bind(&SlashCommandController::onCommandCreated, this, _1));
   }
 }
@@ -72,8 +78,29 @@ void nb::SlashCommandController::onSlashCommand(
   {
     event.reply(std::string("pong"));
   }
+  else if (event.command.get_command_name() == "help")
+  {
+    std::ostringstream oss;
+    oss << "Available nodes and their commands\n";
 
-  if (event.command.get_command_name() == "cmd")
+    for (const auto &node : mNodes)
+    {
+      oss << "Id: " << node.id << ", Name: " << node.info.name;
+
+      if (!node.commandCallbacks.empty())
+      {
+        oss << ", Commands: ";
+        for (const auto & [ key, value ] : node.commandCallbacks)
+        {
+          oss << key << " ";
+        }
+      }
+      oss << "\n";
+    }
+
+    event.reply(oss.str());
+  }
+  else if (event.command.get_command_name() == "cmd")
   {
     // Non-optional parameters
     const auto id = std::get<int64_t>(event.get_parameter("id"));
