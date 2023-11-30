75e92489fa416775ca32cb339a0083ea0d81c132
Casper Thorø Vium Pedersen <kanoo@posteo.de>
2023-11-30 19:24:22 +0100

    Can't remember :-)

--- src/dpp/DppController.cpp
+++ src/dpp/DppController.cpp
@@ -68,12 +68,9 @@ void nb::DppController::onGetGuilds(const dpp::confirmation_callback_t &event)
     for (auto const &[key, value] : guilds)
     {
       mGuild = std::make_unique<dpp::guild>(value);
-      mLogger->info(
-          "I'm a Discord bot named '{}' ({}) connected to guild '{}' ({})",
-          mBot->me.username, mBot->me.id, mGuild->name, mGuild->id);
+      mLogger->info("I'm a Discord bot named '{}' ({}) connected to guild '{}' ({})", mBot->me.username, mBot->me.id, mGuild->name, mGuild->id);
 
-      mBot->start_timer(std::bind(&nb::DppController::onTimerTick, this),
-                        mConfig.updateFrequencySeconds);
+      mBot->start_timer(std::bind(&nb::DppController::onTimerTick, this), mConfig.updateFrequencySeconds);
     }
   }
 }
@@ -180,8 +177,7 @@ void nb::DppController::start()
 {
   if (mBot != nullptr)
   {
-    mLogger->info("Starting DppController in thread {} (Dpp {})", __func__,
-                  std::hash<std::thread::id>{}(std::this_thread::get_id()), dpp::utility::version());
+    mLogger->info("Starting DppController in thread {} (Dpp {})", __func__, std::hash<std::thread::id>{}(std::this_thread::get_id()), dpp::utility::version());
     mBot->start(dpp::st_wait);
   }
 }
