1f784db468f20c8dc2084dbcc7351f279b449ea3
Casper Thorø Vium Pedersen <kanoo@posteo.de>
2023-12-03 10:44:13 +0100

    Started on Nix flake. Have to fix missing mosquittoConfig.cmake for mosquitto nix package.

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "NodeBot, C++ MQTT to Discord proxy.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        # To import a flake module
+        # 1. Add foo to inputs
+        # 2. Add foo as a parameter to the outputs function
+        # 3. Add here: foo.flakeModule
+
+      ];
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        # devShells.default describes the default shell with C++ and libraries.
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            cmake
+            gtest
+            mosquitto
+            ninja
+            nlohmann_json
+            spdlog
+            toml11
+            zlib
+          ];
+        };
+      };
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+
+      };
+    };
+}
